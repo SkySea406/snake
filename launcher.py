5e46f0987dc38cce926bc5b702d48ec408300f15
SkySea406 <66123149+SkySea406@users.noreply.github.com>
2025-04-06 22:44:33 +0300

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,12 +1,19 @@
 import tkinter as tk
 from tkinter import messagebox
 import os
+
+
 def baslat():
     dif = dife.get()
-    if dif.type() != int:
+    
+    if not dif.isdigit():
         messagebox.showerror("Error", "Please enter a valid difficulty level.")
+        
     else:
-        os.system("python snake.py")
+        dir = os.path.dirname(os.path.realpath(__file__))
+        snakedir = os.path.join(dir, "snake.py")
+
+        os.system("python "+snakedir)
         parent.destroy()
         exit()
 parent = tk.Tk()
@@ -14,6 +21,20 @@ parent = tk.Tk()
 parent.title("Launcher")
 parent.geometry("100x100")
 parent.resizable(False, False)
+parent.configure(bg="white")
+
+
+diff = tk.Label(parent, text="Enter Difficulty", bg="white")
+diff.pack()
+dife = tk.Entry(parent, bg="white")
+dife.pack()
+ok = tk.Button(parent, text="OK", command=baslat, bg="white")
+ok.pack()
+
+
+
+
+parent.mainloop()
 parent.configure(bg="white")
 
 
